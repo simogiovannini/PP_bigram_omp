0e52987a6810127838cf0cfb42e9cc734c1a0936
simogiovannini <simone.giovannini1@stud.unifi.it>
2023-03-30 15:36:30 +0200

    Added function to read text from file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+string readInput(string path);
 
 int main() {
+    string corpus = readInput("../corpus.txt");
 
+    cout << corpus.length();
+
+    int ngram_length = 2;
+
+    return 0;
+}
+
+string readInput(string path) {
+    ifstream file("../corpus.txt");
+    string str;
+    string data;
+    while (getline(file, str)) {
+        data += str;
+        // file_contents.push_back('\n');
+    }
+    transform(data.begin(), data.end(), data.begin(), [](unsigned char c){ return std::tolower(c); });
+    return data;
 }
